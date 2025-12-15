602fabde1c8ee4344dabf2dba9741127d6a88bf6
bearinventor-labs <thebearinventor@gmail.com>
2025-12-15 12:30:45 +0100

    Add menu function template in PowerShell

---
+++ templates/functions/menu_functions_template.ps1
@@ -0,0 +1,16 @@
+# <-- PLEASE CHANGE THE VERB-NOUN FUNCTION NAME!!! -->
+
+function name_menu {
+    Clear-Host 
+
+    Write-Host ""
+    Write-Host "================================================" -ForegroundColor Cyan
+    Write-Host "Title/Question/Prompt" -ForegroundColor Cyan
+    Write-Host "-----------------------" -ForegroundColor Cyan # <-- Use this if you need a list of choices! -->
+    Write-Host "1. Option 1" -ForegroundColor Cyan
+    Write-Host "2. Option 2" -ForegroundColor Cyan
+    Write-Host "B. Back" -ForegroundColor Cyan
+    Write-Host "Q. Quit" -ForegroundColor Cyan
+    Write-Host "================================================" -ForegroundColor Cyan
+    Write-Host ""
+}
